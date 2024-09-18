ba9c60c137ecf02800a56554dbe091d0ba263cc9
DylaNSWER06 <akunsmurfh1122@gmail.com>
2024-09-18 16:16:11 +0700

    latihan

---
+++ modul-1/230441100027_DylanAkhtarreza/pemvisujicoba/src/pemvisujicoba/Pemvisujicoba.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pemvisujicoba;
+
+/**
+ *
+ * @author USER
+ */
+public class Pemvisujicoba {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
