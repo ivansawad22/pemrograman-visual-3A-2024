3c3cbc9ff900f09cbe803cb542ed90e21c6f957c
Adiittiiyaa <aditdacosta715@gmail.com>
2024-09-18 16:12:41 +0700

    latihan

---
+++ modul-1/230441100138_Aditya/PRAKVIS1/src/prakvis1/PRAKVIS1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prakvis1;
+
+/**
+ *
+ * @author ADITYA
+ */
+public class PRAKVIS1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
