6dc435117d0339f8ef9fb7eec0f924ac840d0cfe
Mfajrialfaini <alvenlover@gmail.com>
2024-09-30 16:52:30 +0700

    Mengumpulkan modul 1 asprak kak devi

--- modul-1/230441100171_MuhammadFajriAlfaini/src/modul1pemvis/Modul1Pemvis.java
+++ modul-1/230441100171_MuhammadFajriAlfaini/src/modul1pemvis/Modul1Pemvis.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package projekkalian;
+package modul1pemvis;
 
 /**
  *
  * @author Muhammad Fajri
  */
-public class ProjekKalian {
+public class Modul1Pemvis {
 
     /**
      * @param args the command line arguments
---
+++ modul-1/msc/meet1.py
@@ -0,0 +1,21 @@
+# coba = 50
+# apel = 20.5
+# durian = True
+# mangga = "buah"
+
+# makanan = ["bakso", "pecel", "ayam geprek", "indomie"]
+# minuman = ("marimas", "nutrisari", "es kelapa")
+# mahasiswa = {"nama" : "amin"}
+
+# print(type(coba))
+# print(type(apel))
+# print(type(durian))
+# print(type(makanan))
+
+
+a = 1
+b = 6
+c = 8
+
+if (a<b & b<c):
+  print("benar")
