c587b2c6d562647dd1d1407728e5fccb1628c5e9
vikass22233 <vikasirmanwansya@gmail.com>
2024-09-30 21:18:01 +0700

    VikasIrmanWansyah_230441100029_KakIqbal

--- modul-1/230441100029_VikasIrmanWansyah/Modul1/src/modul1/Modul1.java
+++ modul-1/230441100029_VikasIrmanWansyah/Modul1/src/modul1/Modul1.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package belajar1;
+package modul1;
 
 /**
  *
  * @author User
  */
-public class Belajar1 {
+public class Modul1 {
 
     /**
      * @param args the command line arguments
