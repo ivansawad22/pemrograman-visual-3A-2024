d14a72dfdbcda3bd2c61de679d9040a625673d01
ELRUBYAUGUSTINN <elrubyaugustin13@example.com>
2024-10-01 16:47:06 +0700

    230441100004-ElRubyAugustin-MuhammadIqbalFirmansyah-Modul1

--- modul-1/230441100004-ElRubyAugustin/src/modul/pkg1/Modul1.java
+++ modul-1/230441100004-ElRubyAugustin/src/modul/pkg1/Modul1.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package projectruby;
+package modul.pkg1;
 
 /**
  *
  * @author NAOFUMI
  */
-public class ProjectRuby {
+public class Modul1 {
 
     /**
      * @param args the command line arguments
