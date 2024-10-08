14038245b0ee24190251f229ec43c6abe84c9acb
4n994 <anggastrya007@gmail.com>
2024-10-08 23:33:57 +0700

    230441100101_ErlanggaSatryaHusada_Modul2_AsprakKakAkmal

---
+++ modul-2/230441100101_ErlanggaSatryaHusada/TugasModul2_Pemvis/src/tugasmodul2_pemvis/TugasModul2_Pemvis.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugasmodul2_pemvis;
+
+/**
+ *
+ * @author Erlangga Satrya H
+ */
+public class TugasModul2_Pemvis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        try {
+            
+        }catch (NumberFormatException e) {
+            System.out.println("gajadi error  boss");
+        }
+    }
+    
+}
